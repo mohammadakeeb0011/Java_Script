7f2c73d01fa025cf875f4fa17b8ad9609e2b127b
MOHMMAD AKEEB <121107510+mohammadakeeb0011@users.noreply.github.com>
2024-07-17 14:19:03 +0000

    17-07-2024

--- 01_Basic/01_variables.js
+++ 01_Basic/01_variables.js
@@ -1,1 +1,16 @@
-const account
+const accountId = 144553                //constant
+let accountEmail = "example@gmail.com"  // automatic variable
+var accPassword = "12345"               // static variable
+accCity = "Jaipur"                      // string
+
+// accountId = 2 // not allowed
+
+accountEmail = "xyz@gmail.com"
+accPassword = "121212121"
+accCity = "khan"
+
+/* 
+Preffer not to use var
+because of issue in block scope
+*/
+console.table([accountId, accountEmail, accPassword, accCity])
